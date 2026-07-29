ab693304610b52613cbec17f64e1fa00a4e7a60f
vaibhavBhardwaj24 <bhardwajvaibhav2412@gmail.com>
2026-07-29 18:32:38 +0530

    minor fix

--- app/components/ParentPopup.tsx
+++ app/components/ParentPopup.tsx
@@ -41,7 +41,7 @@ export default function ParentPopup() {
 
   return (
     <div
-      className="fixed inset-0 z-50 flex items-center justify-center p-4"
+      className="fixed inset-0 z-[60] flex items-center justify-center p-4"
       role="dialog"
       aria-modal="true"
       aria-labelledby="parent-popup-heading"
